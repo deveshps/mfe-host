6168c0c223cab96d3df1ee545cb64300de94b3fa
Abhishek Raj <abhishekr@dewsolutions.in>
2023-05-13 05:33:13 +0530

    Add assets

--- config/webpack.client.js
+++ config/webpack.client.js
@@ -24,7 +24,7 @@ const plugins = [
         globOptions: {
           ignore: ['**/index.html'],
         },
-        noErrorOnMissing: true,
+        // noErrorOnMissing: true,
       },
     ],
   }),
