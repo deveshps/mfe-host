1dcf81ffe360a58926aafd6ece711a058a17efcc
Abhishek Raj <abhishekr@dewsolutions.in>
2023-06-19 11:52:43 +0530

    Integrate remote2

--- src/utils/assets-map.ts
+++ src/utils/assets-map.ts
@@ -1,6 +1,11 @@
 import assetsMap from '../../dist/webpack.manifest.json';
 import remote1AssetsMap from 'remote1/assetsMap';
+import remote2AssetsMap from 'remote2/assetsMap';
 
-const assetsMapAll = { ...assetsMap, ...remote1AssetsMap };
+const assetsMapAll = {
+  ...assetsMap,
+  ...remote1AssetsMap,
+  ...remote2AssetsMap,
+};
 
 export default assetsMapAll;
