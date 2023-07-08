cd7a9350d29e4cc4ca6b61b9b26c9a9b508339e2
deveshps <deveshps@dewsolutions.in>
2023-07-08 11:37:17 +0530

    change bin for login mfe

--- bin/cli.js
+++ bin/cli.js
@@ -13,26 +13,36 @@ const runCommand = (command) => {
 }
 
 const repoName = process.argv[2];
+const secondRepoName = process?.argv[3] || "mfe-login";
+
+
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/deveshps/mfe-host.git ${repoName}`;
-// const gitCheckoutCommandForMfeTwo = `git clone --depth 1 https://github.com/deveshps/mfe-login.git ${repoName}`;
+const gitCheckoutCommandForMfeTwo = `git clone --depth 1 https://github.com/deveshps/mfe-login.git ${secondRepoName}`;
 
 const installDepsCommand = `cd ${repoName} && npm install`;
 
+const installDepsCommand2 = `cd ${secondRepoName} && npm install`;
 
-console.log("Cloning the repository with name ",repoName);
+
+
+console.log("Cloning the repository with name ",repoName,secondRepoName);
 const checkOut = runCommand(gitCheckoutCommand);
-// const checkOutForMfeTwo = runCommand(gitCheckoutCommandForMfeTwo)
+const checkOutForMfeTwo = runCommand(gitCheckoutCommandForMfeTwo)
 
-if(!checkOut) process.exit(1); // (code : -1)
-// if(!checkOutForMfeTwo) process.exit(1)
+if(!checkOut || !checkOutForMfeTwo) process.exit(1); // (code : -1)
 
-console.log("Installing dependencies for ",repoName);
+console.log("Installing dependencies for ",repoName,secondRepoName);
 
 const installedDeps = runCommand(installDepsCommand);
+const installedDeps2 = runCommand(installDepsCommand2);
 
-if(!installDepsCommand) process.exit(1) // (code : -1)
+
+if(!installDepsCommand || !installDepsCommand2) process.exit(1) // (code : -1)
 
 console.log("Congratulations! You are ready. Follow the following commands to start");
 console.log(`cd ${repoName} && npm start`);
+
+console.log(`cd ${secondRepoName} && npm start`);
+
 
 //npm publish --access=public
